81798e3faa1b759977b4eae1b69f6b1849ca89fa
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2020-12-27 18:45:19 +0100

    Uses gravatar.com for the avatar.

--- lib/bloggerday_web/views/helpers/tailwind_helper.ex
+++ lib/bloggerday_web/views/helpers/tailwind_helper.ex
@@ -10,4 +10,14 @@ defmodule BloggerdayWeb.TailwindHelper do
       )
     end)
   end
+
+  def gravatar_url(email) do
+    hash = email
+      |> String.trim()
+      |> String.downcase()
+      |> :erlang.md5()
+      |> Base.encode16(case: :lower)
+
+    "https://www.gravatar.com/avatar/#{hash}?s=150&d=identicon"
+  end
 end
